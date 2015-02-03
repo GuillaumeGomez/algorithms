81eff548731f4a8a680023f50d12ffdadf85d822
GuillaumeGomez <guillaume1.gomez@gmail.com>
2015-02-03 22:18:55 +0100

    Update presentation

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/time.h>
+#include <time.h>
 
 #include "algo.h"
 
@@ -51,19 +52,28 @@ float profile_function(int *tb, int size, void(*func)(int*, int)) {
     return ((float)(tmp2.tv_sec - tmp.tv_sec)) + (tmp2.tv_usec - tmp.tv_usec) / 1000000.f;
 }
 
+void calls(const char *begin_str, int *array, int size) {
+    printf("\033[36;1m=== Launch with %s: ===\033[0m\n", begin_str);
+    printf("\033[33;1m=> bubble sort: worst = n^2 | best = n | average = n^2\033[0m\n");
+    printf("%f secs\n", profile_function(array, size, bubble_sort));
+    printf("\033[33;1m=> merge sort : worst = n log n | best = n log n | average = n log n\033[0m\n");
+    printf("%f secs\n", profile_function(array, size, merge_sort));
+    printf("\033[33;1m=> quick sort : worst = n^2 | best = n log n | average = n log n\033[0m\n");
+    printf("%f secs\n", profile_function(array, size, quick_sort));
+
+    printf("original qsort: %f secs\n", p(array, size));
+}
+
 int main() {
     int tb[TB_SIZE], i = 0;
 
+    srand(time(NULL));
     for (; i < TB_SIZE; ++i)
         tb[i] = TB_SIZE - i;
     printf("Initialization done.\n");
-    printf("\033[33;1m=> bubble sort: worst = n^2 | best = n | average = n^2\033[0m\n");
-    printf("reversed array: %f secs\n", profile_function(tb, TB_SIZE, bubble_sort));
-    printf("\033[33;1m=> merge sort : worst = n log n | best = n log n | average = n log n\033[0m\n");
-    printf("reversed array: %f secs\n", profile_function(tb, TB_SIZE, merge_sort));
-    printf("\033[33;1m=> quick sort : worst = n^2 | best = n log n | average = n log n\033[0m\n");
-    printf("reversed array: %f secs\n", profile_function(tb, TB_SIZE, quick_sort));
-
-    printf("original qsort: %f secs\n", p(tb, TB_SIZE));
+    calls("reversed array", tb, TB_SIZE);
+    for (i = 0; i < TB_SIZE; ++i)
+        tb[i] = rand() % 100000;
+    calls("random array", tb, TB_SIZE);
     return 0;
 }
